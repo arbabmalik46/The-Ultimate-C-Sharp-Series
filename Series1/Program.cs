f7dcdc834e603c89b040be3413ab0a336b9a8e4d
arbabmalik46 <arbabmalik46@gmail.com>
2023-12-25 14:40:21 +0500

    Asd

---
+++ Series1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Series1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World");
+		}
+	}
+}
