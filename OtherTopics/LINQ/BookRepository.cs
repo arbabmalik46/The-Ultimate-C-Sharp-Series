98ce7d673412c2a325c5f77c8608f6173673b31c
arbabmalik46 <arbabmalik46@gmail.com>
2024-01-01 16:42:57 +0500

    Completed LINQ and Nullable Types

---
+++ OtherTopics/LINQ/BookRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtherTopics
+{
+    public class BookRepository
+    {
+        public IEnumerable<Books> GetBooks()
+        {
+            return new List<Books>()
+            {
+            new Books() { Title = "ADO.NET Step by Step", Price = 5},
+            new Books() { Title = "ASP.NET MVC", Price = 9.99f },
+            new Books() { Title = "ASP.NET Web API", Price = 12 },
+            new Books() { Title = "C# Basic Topics", Price = 7 },
+            new Books() { Title = "C# Advance Topics", Price = 9 },
+            new Books() { Title = "C# Advance Topics", Price = 10 },
+            new Books() { Title = "C# Intermediate Topics", Price = 15 }
+            };
+        }
+    }
+}
---
+++ OtherTopics/Program.cs
@@ -0,0 +1,64 @@
+﻿using OtherTopics;
+using System;
+
+namespace OtherTopics
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+            #region LINQ
+            //var books = new BookRepository().GetBooks();
+            #region LINQ Example
+            ////LINQ Query Operators
+            //var cheaperBooks = 
+            //    from b in books
+            //    where b.Price < 5
+            //    orderby b.Price
+            //    select b.Title;
+            //// LINQ Extension Methods
+            //var cheapBooks = books
+            //    .Where(x=>x.Price<10)
+            //    .OrderBy(x=>x.Price)
+            //    .Select(p=>p.Title); //where is used for condition
+            //foreach(var book in cheapBooks)
+            //{
+            //    Console.WriteLine(book);
+            //}
+            //foreach (var item in cheapBooks)
+            //{
+            //    Console.WriteLine("{0} is a cheap book and it's price is {1}", item.Title, item.Price);
+            //}
+            //var cheapBooks = new List<Books>();
+            #region Conventional Way
+            //foreach (var item in books) --This method is old and time consuming and it will take more memory too
+            //{
+            //	if(item.Price<10)
+            //		cheapBooks.Add(item);
+            //}
+            //foreach (var item in cheapBooks)
+            //{
+            //             Console.WriteLine("{0} is a cheap book and it's price is {1}",item.Title,item.Price);
+            //         }
+            #endregion
+            #endregion
+            //var book = books.First(x => x.Title == "C# Advance Topics");
+            //var summedPrice = books.Sum(x=>x.Price);
+            ////var book = books.SingleOrDefault(p=>p.Title == "ASP.NET MVC++");
+            //Console.WriteLine(summedPrice);
+            #endregion
+
+            #region Nullable
+            Nullable<DateTime> dateTime = null;
+            DateTime? date = DateTime.Today ;
+            Console.WriteLine("GetValueOrDefault(): " + date.GetValueOrDefault());
+            Console.WriteLine("HasValue: "+date.HasValue);
+            Console.WriteLine("Value: "+date.Value);
+
+            //null coalescing operator c#
+            DateTime dateTime1 = date ?? DateTime.MaxValue;
+            #endregion
+
+        }
+    }
+}
