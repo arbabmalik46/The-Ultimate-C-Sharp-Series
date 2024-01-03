880c5d8e8a197b64181ecd82eb1806ad5e7dd323
arbabmalik46 <arbabmalik46@gmail.com>
2024-01-04 00:47:39 +0500

    Added A new Project of Unit Test, Wrote First Unit Test, Tested a Method with different Scenarios, installed new packages

---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,34 @@
+﻿
+using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ReservationTests
+    {
+        [Test]
+        public void CanBeCancelledBy_UserIsAdmin_True()
+        {
+            //Arrange
+            var reservationObj = new Reservation();
+            //Act
+            var result = reservationObj.CanBeCancelledBy(new User() { IsAdmin = true });
+            //Assert
+            Assert.IsTrue(result);
+            Assert.That(result, Is.True);
+            Assert.That(result == true);
+        }
+        [Test]
+        public void CanBeCancelBy_UserIsAdmin_False()
+        {
+            //arrange
+            var reqesut = new Reservation();
+            //act
+            var result = reqesut.CanBeCancelledBy(new User() { IsAdmin = false});
+            //assert
+            Assert.IsFalse(result);
+        }
+    }
+}
